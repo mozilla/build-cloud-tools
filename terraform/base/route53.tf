96e088c12ab5e3db05438f6289239c766f9ab1c7
Sylvestre Ledru <sylvestre@mozilla.com>
2018-06-03 23:24:13 +0200

    Create relman-ci.moz.tools

--- terraform/base/route53.tf
+++ terraform/base/route53.tf
@@ -122,6 +122,19 @@ resource "aws_route53_record" "heroku-coverage-cname-stage" {
     records = ["coverage.staging.moz.tools.herokudns.com"]
 }
 
+#############################
+##  moz.tools other cnames ##
+#############################
+
+resource "aws_route53_record" "relman-ci-moz-tools-cname-prod" {
+    zone_id = "${aws_route53_zone.moztools.zone_id}"
+    name = "relman-ci.moz.tools"
+    type = "CNAME"
+    ttl = "180"
+    records = ["ec2-35-180-75-156.eu-west-3.compute.amazonaws.com"]
+}
+
+
 #########################################
 ## Heroku Shipit production app cnames ##
 #########################################
